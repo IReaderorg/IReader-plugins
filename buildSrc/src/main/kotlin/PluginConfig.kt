c6957839e995a7aaa86e6d2d32403a89b56f4c27
kazem <kazem.codes@gmail.com>
2025-12-21 18:53:10 +0330

    update

--- buildSrc/src/main/kotlin/PluginConfig.kt
+++ buildSrc/src/main/kotlin/PluginConfig.kt
@@ -6,7 +6,7 @@ object PluginConfig {
     
     // Plugin API version this project targets
     // Must match the version published in IReader's plugin-api module
-    const val pluginApiVersion = "1.0.6"
+    const val pluginApiVersion = "1.0.7"
     
     // IReader minimum version for plugins
     const val minIReaderVersion = "1.0.0"
